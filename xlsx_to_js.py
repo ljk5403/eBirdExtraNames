d6c7ffbb116e6f1491ac1ea1e57af1cf478e26f4
JiankunLi <ljk5403@outlook.com>
2025-06-23 19:47:02 -0500

    Modification for Multilanguage

--- xlsx_to_js.py
+++ xlsx_to_js.py
@@ -6,6 +6,11 @@ js_nameMap = 'js_nameMap.txt'
 
 referanceDf = pd.read_excel(referance)
 
+print("Found Columns")
+print(referanceDf.columns)
+
+targetColumn="Chinese"
+
 
 # Open a file in write mode
 with open(js_nameMap, "w") as file:
@@ -13,8 +18,9 @@ with open(js_nameMap, "w") as file:
     for index, row in referanceDf.iterrows():
         # Fetch the elements in "IOC_xx.x" and "Chinese" columns
         ioc_value = row["IOC_15.1"]
-        chinese_value = row["Chinese"]
+        chinese_value = row[targetColumn]
         # Format the string as "A": "B",
         formatted_string = f'"{ioc_value}": "{chinese_value}",\n'
         # Write to the file
         file.write(formatted_string)
+    print("JS namemap file  generated:", js_nameMap)
